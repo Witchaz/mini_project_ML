2334babbfad4a9e045b629942ec943763498d3a2
Witchaz <Jengkug23@gmail.com>
2024-03-09 00:35:59 +0700

    implement UI

--- app.py
+++ app.py
@@ -5,45 +5,47 @@ import joblib
 
 # "Artifacts"
 pipeline = joblib.load("pipeline.joblib")
-label_pipeline = joblib.load("label_pipeline.joblib")
-departments = joblib.load("departments.joblib")
-salary = joblib.load("salary.joblib")
+# label_pipeline = joblib.load("label_pipeline.joblib")
+departments_list = joblib.load("departments.joblib")
+salary_list = joblib.load("salary.joblib")
+boolean_dict = {'Yes':1,'No':0}
 
 
 def predict(department, promoted, review, projects, salary, tenure, satisfaction, bonus, avg_hrs_month):
     sample = dict()
     sample['department'] = department
-    sample['promoted'] = promoted
+    sample['promoted'] = boolean_dict[promoted]
     sample['review'] = review
     sample['projects'] = projects
     sample['salary'] = salary
     sample['tenure'] = tenure
     sample['satisfaction'] = satisfaction
-    sample['bonus'] = bonus
+    sample['bonus'] = boolean_dict[bonus]
     sample['avg_hrs_month'] = avg_hrs_month
 
     left = pipeline.predict(pd.DataFrame([sample]))
-    left = label_pipeline.inverse_transform([price])
     return left
 
 # https://www.gradio.app/guides
 with gr.Blocks() as blocks:
-    department = gr.Dropdown(departments, value=departments[0], label="Department")
-    promoted = gr.Textbox(label="Promoted")
-    review = gr.Number(label="Review", value=1, minimum=0.0, maximum= 1)
+    department = gr.Dropdown(departments_list, value=departments_list[0], label="Department")
+    promoted = gr.Radio(['Yes','No'],label = "promoted",info = "is they promoted?")
+    review = gr.Number(label="Review", value=0.0, minimum=0.0, maximum= 1.0,step=0.1,info="range(0-1)")
     projects = gr.Number(label="Projects", value=1, minimum=0, step=1)
-    salary = gr.Dropdown(salary, value=salary[0], label="salary")
+    salary = gr.Dropdown(salary_list, value=salary_list[0], label="salary")
     tenure = gr.Number(label="Tenure", value=1, minimum=0)
-    satisfaction = gr.Number(label="Satisfaciton", value=1, minimum=0.0, maximum= 1)
-    bonus = gr.Number(label="Bonus", value=1, minimum=0, maximum= 1, step=1)
-    avg_hrs_month = gr.Slider(label="Average hours/month", minimum=0)
-    left = gr.Text(label="Price")
+    satisfaction = gr.Number(label="Satisfaciton", value=0.0, minimum=0.0, maximum= 1.0,step=0.1,info="range(0-1)")
+    bonus = gr.Radio(['Yes','No'],label = "bonus",info = "is they get bonus?")
+    avg_hrs_month = gr.Number(label="Average hours/month", minimum=0)
+    left = gr.Text(label="left")
 
     inputs = [department, promoted, review, projects, salary, tenure, satisfaction, bonus, avg_hrs_month]
-    outputs = [left]
+    outputs = left
 
     predict_btn = gr.Button("Predict")
-    predict_btn.clieck(predict, inputs=inputs, outputs=outputs)
+    predict_btn.click(predict, inputs=inputs, outputs=outputs)
+
+    
 
 if __name__ == "__main__":
     blocks.launch() # Local machine only
